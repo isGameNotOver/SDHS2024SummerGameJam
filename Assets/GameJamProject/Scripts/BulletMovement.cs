0ea084720cb29644efbe592a2b0d50406f00698d
jungminjun <juengminjun824@gmail.com>
2024-07-20 23:55:33 +0900

    BulletFix

---
+++ Assets/GameJamProject/Scripts/BulletMovement.cs
@@ -0,0 +1,20 @@
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    [SerializeField] private float bulletMoveSpeed = default;
+
+    private Vector2 direction;
+
+    public void SetDirection(Vector2 direction)
+    {
+        this.direction = direction;
+    }
+
+    public void Update()
+    {
+        transform.position += (Vector3)direction * bulletMoveSpeed * Time.deltaTime;
+    }
+
+}
